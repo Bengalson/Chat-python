fa523bb5b61ce5ba589d78be9459220356e1f0c9
Bengalson <80784350+Bengalson@users.noreply.github.com>
2024-06-19 04:55:45 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,7 +1,12 @@
 import socket
 import threading
+from colorama import init, Fore, Style
+
+# Initialize colorama
+init(autoreset=True)
 
 def receive_messages(client_socket):
+    """Receive messages from the server and display them"""
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
@@ -13,24 +18,32 @@ def receive_messages(client_socket):
             break
 
 def send_message(client_socket):
+    """Send messages to the server"""
     while True:
         message = input("")
-        client_socket.send(message.encode('utf-8'))
+        try:
+            client_socket.send(message.encode('utf-8'))
+        except:
+            break
 
 def connect_to_server():
+    """Connect to the server"""
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 12345))
     return client_socket
 
 def start_receiving_thread(client_socket):
+    """Start a thread for receiving messages"""
     receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
     receive_thread.start()
 
 def start_sending_thread(client_socket):
+    """Start a thread for sending messages"""
     send_thread = threading.Thread(target=send_message, args=(client_socket,))
     send_thread.start()
 
 def close_connection(client_socket):
+    """Close the connection to the server"""
     client_socket.close()
 
 if __name__ == "__main__":
